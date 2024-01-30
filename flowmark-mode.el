e3adc7570c8dffbd7a417ca44c8aabc014cf6f76
Zetian Lin <bctnry@outlook.com>
2024-01-30 08:01:14 +0800

    fix global escape

--- flowmark-mode.el
+++ flowmark-mode.el
@@ -7,12 +7,12 @@
 (defvar flowmark-syntax-coloring nil)
 (setq flowmark-syntax-coloring
       '(("\\\\\s*\(\(.*\)\)\s*" . font-lock-comment-face)
-	("\\(\\\\\\(?:call\\|def\\(?:\.free\\)?\\)\\)\(\\(\\(?:[^,\\]\\|\\\\,\\)+\\)"
+	("\\(\\\\\\(?:call\\|def\\(?:\.free\\)?\\)\\)\(\\(\\(?:[^,@]\\|@,\\|@@\\)+\\)"
 	 (1 font-lock-keyword-face)
 	 (2 font-lock-function-name-face))
 	("\\(\\\\\\(?:halt\\|debug\.list_names\\|init\.macro\\|copy\\|move\\|del\\(?:\.\\(?:free\\|all\\)\\)?\\|recite\.\\(?:reset\\|n?char\\|next_piece\\|to_gap\\|to_pattern\\)\\|next\.\\(?:piece\\|char\\|line\\)\\|if\\(?:eq\\|ne\\)\\(?:\.\\(?:int\\|float\\)\\)?\\)\\)"
 	 (1 font-lock-keyword-face))
-	("\\(\\\\[^\n(]+\\)\("
+	("\\(\\\\\\(?:[^@(]\\|@(\\|@@\\)+\\)\("
 	 (1 font-lock-function-name-face))
 	("<[^>]*>" . font-lock-variable-name-face)
 	("\\\\\s*\n\s*"
