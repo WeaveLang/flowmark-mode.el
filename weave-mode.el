fa5a3a2c8103eba7155484b162c1105a3342310a
Zetian Lin <bctnry@outlook.com>
2024-02-22 19:35:32 +0800

    Update and rename flowmark-mode.el to weave-mode.el

--- weave-mode.el
+++ weave-mode.el
@@ -1,14 +1,14 @@
-;; flowmark-mode.el
-;; Major mode for Flowmark.
+;; weave-mode.el
+;; Major mode for Weave.
 ;; (c) 2024 Zahktïn Làhm
 ;;
 ;; This file is not part of GNU Emacs.
 
-(defvar flowmark-syntax-coloring nil)
-(setq flowmark-syntax-coloring
+(defvar weave-syntax-coloring nil)
+(setq weave-syntax-coloring
       '(("\\\\\s*\n\s*" . font-lock-comment-face)
 	("\\\\((\\(?:[\n]\\|.\\)*))" . font-lock-comment-face)
-	("\\(\\\\\\(?:call\\|def\\(?:\.\\(?:free\\|macro\\|keyword\\)\\)?\\)\\)\(\\(\\(?:[^,@()]\\|@,\\|@@\\|@(\\|@)\\)+\\)"
+	("\\(\\\\\\(?:call\\|import\\|path\\|def\\(?:\.\\(?:free\\|macro\\|keyword\\)\\)?\\)\\)\(\\(\\(?:[^,@()]\\|@,\\|@@\\|@(\\|@)\\)+\\)"
 	 (1 font-lock-keyword-face)
 	 (2 font-lock-function-name-face))
 	("\\(\\\\\\(?:halt\\|debug\.list_names\\|init\.macro\\|copy\\|move\\|del\\(?:\.\\(?:free\\|all\\)\\)?\\|recite\.\\(?:reset\\|n?char\\|next_piece\\|to_gap\\|to_pattern\\)\\|next\.\\(?:piece\\|char\\|line\\)\\|if\\(?:eq\\|ne\\)\\(?:\.\\(?:int\\|float\\)\\)?\\|out\\|error\\|warn\\)\\)"
@@ -17,8 +17,8 @@
 	 (1 font-lock-function-name-face))
 	("<[^>]*>" . font-lock-variable-name-face)))
 
-(define-derived-mode flowmark-mode fundamental-mode "Flowmark"
-  "Major mode for Flowmark"
-  (setq font-lock-defaults '((flowmark-syntax-coloring))))
+(define-derived-mode weave-mode fundamental-mode "Weave"
+  "Major mode for Weave"
+  (setq font-lock-defaults '((weave-syntax-coloring))))
 
-(provide 'flowmark-mode)
+(provide 'weave-mode)
